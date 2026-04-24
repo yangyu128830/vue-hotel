89d1068815e20526e322623726422fa88ba5a064
ysy13 <ysy13044878810@qq.com>
2026-04-24 13:22:39 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/page/index/Index'
+import HotelList from '@/page/hotel-list/index'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/hotel-list',
+      name: 'HotelList',
+      component: HotelList
     }
   ]
 })
